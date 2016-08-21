5320e47b5bfbfb8c7e658bc70935d94b989e0d01
qhackerjj@163.com <qhackerjj@163.com>
2016-08-21 14:38:21 +0800

    新建项目admin

---
+++ PlatAdmin/PlatAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlatAdmin.Startup))]
+namespace PlatAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
